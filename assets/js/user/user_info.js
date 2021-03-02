a787fcc38afc442613366919bd62804d81efcce0
gaoxinwei <gaoxinwei0112@163.com>
2021-03-02 10:16:00 +0800

    完成了个人中心开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,52 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function (value) {
+            if (value > 6) {
+                return '昵称长度必须在1~6个字符之间'
+            }
+        }
+    })
+
+    initUserInfo()
+    //初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            type: "GET",
+            url: "/my/userinfo",
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败')
+                }
+                //调用foem.val()快速为表单赋值
+                // form.val('formUserInfo',res.data)
+                form.val('formUserInfo', res.data)
+            }
+        });
+    }
+
+    //阻止表单的默认重置行为
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    //监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        //阻止默认行为
+        e.preventDefault()
+        $.ajax({
+            type: "POST",
+            url: "/my/userinfo",
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新用户信息失败！')
+                }
+                layui.layer.msg('更新用户信息成功')
+                window.parent.getUserInfo()
+            }
+        });
+    })
+})
